40760edc349740a5feea41eec106448425f7626a
thnquang0203 <tranquang202032006@gmail.com>
2026-07-17 10:42:33 +0700

    khoi tao cau truc ShopBadminton

---
+++ backend/ShopBadminton/src/main/java/com/shopbadminton/ShopBadmintonApplication.java
@@ -0,0 +1,13 @@
+package com.shopbadminton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBadmintonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBadmintonApplication.class, args);
+	}
+
+}
